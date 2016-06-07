82525ce8cd18c674f56592be9b3f24dc040b0b86
Douglas Duteil <douglasduteil@gmail.com>
2016-06-07 23:01:14 +0200

    feat(all): write the tests

--- packages/plugin-json/test/test.js
+++ packages/plugin-json/test/test.js
@@ -1,7 +1,22 @@
 /* eslint-env mocha */
 
+var SystemJS = require('systemjs')
+var chai = require('chai')
+var path = require('path')
+
+var expect = chai.expect
+
 describe('plugin-json', function () {
+  before(function () {
+    System.baseURL = path.resolve(__dirname, '..')
+    System.map['json'] = 'plugin-json.js'
+  })
+
   it('should return the file as json', function () {
-    throw new Error('TODO')
+    var actualFilePath = path.resolve(__dirname, 'actual.json')
+    return SystemJS.import(actualFilePath + '!')
+      .then(function (actual) {
+        expect(require(actualFilePath)).to.eql(actual)
+      })
   })
 })
--- packages/plugin-text/test/test.js
+++ packages/plugin-text/test/test.js
@@ -1,7 +1,26 @@
 /* eslint-env mocha */
 
+var SystemJS = require('systemjs')
+var chai = require('chai')
+var chaiFiles = require('chai-files')
+var path = require('path')
+
+chai.use(chaiFiles)
+
+var expect = chai.expect
+var file = chaiFiles.file
+
 describe('plugin-text', function () {
+  before(function () {
+    System.baseURL = path.resolve(__dirname, '..')
+    System.map['txt'] = 'plugin-text.js'
+  })
+
   it('should return the file as pure test', function () {
-    throw new Error('TODO')
+    var actualFilePath = path.resolve(__dirname, 'actual.txt')
+    return SystemJS.import(actualFilePath + '!')
+      .then(function (actual) {
+        expect(file(actualFilePath)).to.equal(actual)
+      })
   })
 })
