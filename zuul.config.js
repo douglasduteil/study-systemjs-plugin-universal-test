f9a2019a9f68f00692d0b37c711b68d20f62447a
Douglas Duteil <douglasduteil@gmail.com>
2017-01-10 04:16:20 +0100

    take advance of the travis.config.js

--- zuul.config.js
+++ zuul.config.js
@@ -1,7 +1,26 @@
 'use strict'
 
+const {
+  BROWSER_NAME,
+  BROWSER_VERSION,
+  COMMIT_SHA1,
+  CI,
+  TRAVIS_BRANCH,
+  TRAVIS_JOB_NUMBER
+} = process.env
+
+const name = `${TRAVIS_BRANCH} (${COMMIT_SHA1}) - ${TRAVIS_JOB_NUMBER}`
+
 module.exports = {
+  name: CI ? name : 'Local cloud launch > ' + new Date(),
   browserify: [
     {transform: 'brfs'}
-  ]
+  ],
+  browsers: CI && [
+    {
+      name: BROWSER_NAME,
+      version: BROWSER_VERSION
+    }
+  ],
+  ui: 'tape'
 }
